82e01692e641ccb35ec1cdb13155295ea9b4cbaf
Mat-Carv <mat.candido2012@gmail.com>
2022-02-21 03:31:16 +1100

    Added placeholder image; Remove successfully returns image to placeholder.

--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -1,19 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import * as s from '../styles/UserUpdateStyles';
+import img from '../data/placeholder.jpeg'
 
 const ProfilePic = () => {
     const uploadedImage = React.useRef(null);
+    const imageUploader = React.useRef(null);
+    const {current} = uploadedImage;
 
     const handleRemove = () => {
-        console.log("yeah it works")
+        current.src = img
     };
 
     const handleImageUpload = (e) => {
         const [file] = e.target.files;
         if (file) {
         const reader = new FileReader();
-        const {current} = uploadedImage;
         current.file = file;
         reader.onload = (e) => {
             current.src = e.target.result;
@@ -21,11 +22,24 @@ const ProfilePic = () => {
         reader.readAsDataURL(file);
         }
     };
+    
     return (
     <s.ProfilePictureContainer>
         <s.Label id="piclabel">Image</s.Label>
-        <input type="file" accept="image/*" onChange={handleImageUpload} />
-        <s.ProfilePic ref={uploadedImage}></s.ProfilePic>
+        <input 
+            type="file" 
+            accept="image/*" 
+            onChange={handleImageUpload}
+            ref={imageUploader}
+            style={{
+            display: "none"
+            }} 
+        />
+        <s.ProfilePic 
+            src={img}
+            ref={uploadedImage}
+            onClick={() => imageUploader.current.click()}
+        ></s.ProfilePic>
         <s.RemoveButton onClick={handleRemove}>Remove</s.RemoveButton>
     </s.ProfilePictureContainer>
     )
